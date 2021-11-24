0d0e737c014226268ec772eb2ae408df630132da
taejiL <77141695+taejiL@users.noreply.github.com>
2021-11-24 09:10:25 +0000

    BOJ #2588 472*385

--- index.js
+++ index.js
@@ -4,16 +4,20 @@ let input = fs.readFileSync('input.txt').toString().split('\n');
 // let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
 //input은 input.txt를 읽어서 줄 별로 나누어서 배열에 담은 것
-//input = ['5 8 4']
+//input = ['4 7 2'] ['3 8 5']
 
-data = input[0].split(' ');//첫 번째 줄의 데이터를 공백 기준 나눔
-// data = ['5', '8', '4']
+data1 = input[0];//첫 번째 줄의 데이터
+data2 = input[1].split('');//두 번째 줄의 데이터를 나눔
+// data1 = ['472'] data2 = ['3', '8', '5']
 
-A = Number(data[0]);
-B = Number(data[1]);
-C = Number(data[2]);
+A = Number(data1); 472
+B = (data2); // 3, 8, 5
 
-console.log((A+B)%C);
-console.log(((A%C) + (B%C))%C);
-console.log((A*B)%C);
-console.log(((A%C) * (B%C))%C);
+B_1 = Number(B[2]); // 5
+B_10 = Number(B[1]); // 8
+B_100 = Number(B[0]); // 3
+
+console.log(A*B_1);
+console.log(A*B_10);
+console.log(A*B_100);
+console.log(Number(A*input[1]));
