f1cf7dacc2deadc1808d7fc826155ca3ddb485ba
taejiL <>
2021-11-24 04:22:15 +0000

    BOJ #1000 A+B

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-<!-- BOJ #10171 고양이 -->
-console.log('\\    /\\');
-console.log(' )  ( \')');
-console.log('(  /  )');
-console.log(' \\(__)|');
+let fs = require('fs');
+let input = fs.readFileSync('input.txt').toString().split('\n');
+// let input = fs.readFileSync('input.txt').toString().split('\n');
+//let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
+//input은 input.txt를 읽어서 줄 별로 나누어서 배열에 담은 것
+//input = ['1 2']
+
+data = input[0].split(' ');//첫 번째 줄의 데이터를 공백 기준 나눔
+// data = ['1', '2']
+
+a = Number(data[0]);
+b = Number(data[1]);
+
+console.log(a + b);
