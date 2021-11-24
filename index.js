80faf5e34f8e3f0b1e0a009aeecdfbeba7a27f54
taejiL <>
2021-11-24 04:31:43 +0000

    BOJ #10869 +-*/%

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 let fs = require('fs');
 let input = fs.readFileSync('input.txt').toString().split('\n');
 // let input = fs.readFileSync('input.txt').toString().split('\n');
-//let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+// let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
 //input은 input.txt를 읽어서 줄 별로 나누어서 배열에 담은 것
-//input = ['1 2']
+//input = ['7 3']
 
 data = input[0].split(' ');//첫 번째 줄의 데이터를 공백 기준 나눔
-// data = ['1', '2']
+// data = ['7', '3']
 
-a = Number(data[0]);
-b = Number(data[1]);
+A = Number(data[0]);
+B = Number(data[1]);
 
-console.log(a + b);
+console.log(A + B);
+console.log(A - B);
+console.log(A * B);
+console.log(parseInt(A/B));
+console.log(A % B);
