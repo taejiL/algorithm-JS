ab131e8f912d8432cfee23786135f1790764e5a1
taejiL <77141695+taejiL@users.noreply.github.com>
2021-11-25 10:00:18 +0000

    BOJ #9498 if문 ABC score

--- index.js
+++ index.js
@@ -5,17 +5,21 @@ let input = fs.readFileSync('input.txt').toString().split('\n');
 
 //input은 input.txt를 읽어서 줄 별로 나누어서 배열에 담은 것
 
-//A와 B는 공백 한 칸으로 구분되어져 있다.
-A = Number(input[0].split(' ')[0]);//첫 번째 줄의 1데이터
-B = Number(input[0].split(' ')[1]);//첫 번째 줄의 2데이터
-// A = ['1'] B = ['2']
+//첫째 줄에 시험 점수. 점수는 0보다 크거나 같고, 100보다 작거나 같은 정수이다.
+score = Number(input[0]);//첫 번째 줄의 1데이터
 
-if ( A > B ) {
-  console.log('>');
+if(score >= 90 && score <= 100){
+  console.log('A')
 }
-else if ( A < B ) {
-  console.log('<');
+else if(score >= 80 && score <= 89){
+  console.log('B')
 }
-else {
-  console.log('==');
+else if(score >= 70 && score <= 79){
+  console.log('C')
+}
+else if(score >= 60 && score <= 69){
+  console.log('D')
+}
+else{
+  console.log('F')
 }
