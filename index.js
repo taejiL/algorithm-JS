abf09be6d71bd1652543cc9d1b9c7ea02be91893
taejiL <77141695+taejiL@users.noreply.github.com>
2021-11-29 05:38:27 +0000

    BOJ #15552 A+B 빠른출력

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ let input = fs.readFileSync('input.txt').toString().split('\n');
 // let input = fs.readFileSync('input.txt').toString().split('\n');
 // let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
-let N = input[0].split(' ');
-let sum = 0; // sum 초기화
+let T = input[0].split(' '); //개수
+let answer = ""; //하나의 문자열에 담기
 
-
-  for(let i = 1; i <= N; i++){
-    sum += i //sum = sum + i
+  for(let i = 1; i <= T; i++){
+    let ABC = input[i].split(' '); // A와 B
+    A = Number(ABC[0]);
+    B = Number(ABC[1]);
+    answer += A + B + "\n";
+    //console.log(`${A+B}`)//콘솔을 적게 출력해야 한다. 
   }
-  console.log(sum) // 밖에서 찍어줘 마지막 한번만 출력
+ console.log(answer);
+ //하나의 문자열에 담아 마지막에 한번만 출력하도록 한다.(빠르게 수행)
 
 
