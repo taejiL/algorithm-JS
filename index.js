2d2b4736e31e08e356b0be2d71ad91c6b5c97580
taejiL <77141695+taejiL@users.noreply.github.com>
2021-11-29 03:25:50 +0000

    BOJ #10950 A+B (*T줄)

--- index.js
+++ index.js
@@ -3,18 +3,13 @@ let input = fs.readFileSync('input.txt').toString().split('\n');
 // let input = fs.readFileSync('input.txt').toString().split('\n');
 // let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
-let alarm = input[0].split(' ');
-//첫 번째 줄의 데이터를 공백 기준 나눔 (0 ≤ H ≤ 23, 0 ≤ M ≤ 59)
-H = Number(alarm[0]);  // 시간
-M = Number(alarm[1]);  // 분
+let T = input[0].split(' ');
+//T 케이스 개수 
 
-if ( M < 45 ) {
-  H -= 1;
-  M += 15; // M +60 -45
-  if (H === -1){
-    H = 23;
-  } 
-  console.log(`${H} ${M}`);
-} else {
-  console.log(`${H} ${M-45}`);
+
+for(let i = 1; i <= T; i++){
+  let ABC = input[i].split(' '); //각 줄의 A와B를 넣는다.
+  console.log(`${Number(ABC[0]) + Number(ABC[1])}`) //A + B
 }
+
+
