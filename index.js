a6ee9a03f4b793fb7554a755765141ba772708d5
taejiL <77141695+taejiL@users.noreply.github.com>
2021-11-29 06:34:16 +0000

    BOJ #2741 빠른N찍기

--- index.js
+++ index.js
@@ -3,17 +3,11 @@ let input = fs.readFileSync('input.txt').toString().split('\n');
 // let input = fs.readFileSync('input.txt').toString().split('\n');
 // let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
-let T = input[0].split(' '); //개수
-let answer = ""; //하나의 문자열에 담기
+let N = input[0].split(' '); //주어진 N
+let answer = ""; // 초기화
 
-  for(let i = 1; i <= T; i++){
-    let ABC = input[i].split(' '); // A와 B
-    A = Number(ABC[0]);
-    B = Number(ABC[1]);
-    answer += A + B + "\n";
-    //console.log(`${A+B}`)//콘솔을 적게 출력해야 한다. 
-  }
- console.log(answer);
- //하나의 문자열에 담아 마지막에 한번만 출력하도록 한다.(빠르게 수행)
-
-
+for( i=1; i<=N; i++ ){
+  answer += i + '\n'; // 빨리
+  //출력을 하나씩 해주면 시간 초과가 난다. 
+}
+console.log(answer);//마지막에 한번에 출력
