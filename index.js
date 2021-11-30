2f66e3c38b65bd5e88ccade3c47b9a97525c6d68
taejiL <77141695+taejiL@users.noreply.github.com>
2021-11-30 05:25:02 +0000

    BOJ #2439 별 찍기-2

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
-let fs = require('fs');
-let input = fs.readFileSync('input.txt').toString().split('\n');
-// let input = fs.readFileSync('input.txt').toString().split('\n');
-// let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+const readline = require('readline');
 
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
 
-  let message = ''; // 초기화
+rl.on('line', function (line) {
+    
 
-  for (let i = 0; i < input[0]; i++) { //input[0]첫째줄 처음에 들어온것(명확히)
-  message += '*'; // 한바퀴씩 돌때마다 *를 더해준다.
-  console.log(message);
-  }
+    let stars = '';
+    for(let stars = 1; stars <= line; stars++){
+        console.log(' '.repeat(line - stars) + '*'.repeat(stars))
+    }
+    console.log(stars);
+    
+    
+rl.close();
+}).on('close', function () {
+    process.exit();
+});
+
+//위의 내용...fs 모듈에서는 틀렸습니다 떴음 ㅠ//
+//아래 나용...  런타임 에러 // 
+// for(let i=0; i<N; i++){// N번째 줄까지, height
+//     let blanks = ''; //초기화
+//     let stars = '';  //초기화
+
+//     for(let b = N-1; b>i; b--){//(N-1)개의 공백으로 시작해서 1씩 감소
+//         blanks += ' '; // blanks에 공백 담기
+//     }
+//     for(let s=0; s<=i; s++){//0번째부터 i번째까지 1씩 증가
+//         stars += '*'; // stars에 * 담기
+//     }
+//     console.log(blanks+stars);//출력
